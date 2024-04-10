613dea139077a2ee4e4032bf82d51fe2418cb981
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2024-04-10 13:26:11 +0400

    Update script.js

--- Chapter-5-Embedded Content/Exercises/Embed Video (Mandatory)/script.js
+++ Chapter-5-Embedded Content/Exercises/Embed Video (Mandatory)/script.js
@@ -1,21 +1,24 @@
-    
-    document.addEventListener("DOMContentLoaded", function() {
-    
-    var video = document.getElementById("musicVideo");
-    var playButton = document.getElementById("playButton");
-    var posterImage = document.getElementById("videoPoster");
+//fully loads dom content before executing code
+document.addEventListener("DOMContentLoaded", function() {
 
-    function togglePlayPause() {
-        if (video.paused || video.ended) {
-            video.play();
-            playButton.textContent = "❚❚";
-            posterImage.style.display = "none";
-        } else {
-            video.pause();
-            playButton.textContent = "▶";
-            posterImage.style.display = "block";
-        }
+//gets references to video element, play button, and poster image from html doc
+var video = document.getElementById("musicVideo");
+var playButton = document.getElementById("playButton");
+var posterImage = document.getElementById("videoPoster");
+
+//function to toggle between playing and pausing video when play button is clicked
+function togglePlayPause() {
+    if (video.paused || video.ended) { //checks if vid is paused or ended
+        video.play(); //if paused or ended, play
+        playButton.textContent = "❚❚"; //changes play button text content
+        posterImage.style.display = "none"; //hides poster image when video starts playing
+    } else {
+        video.pause(); //if video is playing, pause
+        playButton.textContent = "▶"; //changes play button text content
+        posterImage.style.display = "block"; //shows poster image when paused
     }
+}
 
-    playButton.addEventListener("click", togglePlayPause);
+playButton.addEventListener("click", togglePlayPause); //triggers toggleplaypause function when clicked
+
 });
